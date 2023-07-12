1534ecbcb095877fccc79649929e806fd0b65755
sooheyon <shjo4732@gmail.com>
2023-07-13 08:35:10 +0900

    typescript, scss

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,6 +58,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-feed`,
       options: {
