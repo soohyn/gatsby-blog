fc93b69d7e64ede06c7243afd89bf64d6bef87f9
sooheyon <shjo4732@gmail.com>
2023-07-20 08:20:22 +0900

    작업할 컴포넌트 파악 및 생성

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const Footer = () => {
+  return <FooterStyled>index</FooterStyled>
+}
+
+const FooterStyled = styled.footer``
+
+export default Footer
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const Layout = () => {
+  return <LayoutStyled>index</LayoutStyled>
+}
+
+const LayoutStyled = styled.div``
+
+export default Layout
