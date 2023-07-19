dbd6b4c67d255c76950a188550258c6daf41f6e8
sooheyon <shjo4732@gmail.com>
2023-07-19 12:47:48 +0900

    header랑 bio에 들어갈 데이터 작성

---
+++ src/configs/bio.ts
@@ -0,0 +1,12 @@
+export default {
+  title: "Dev.Soo",
+  description: "Front-End-Developer Blog",
+  author: "Soohyeon Jo",
+  profileImage: "", //!
+  introduce: "Front-End-Developer",
+  techSkill: ["React", "JavaScript", "TypeScript"],
+  location: "Seoul, Republic of Korea",
+  email: "sooheyonjo@gmail.com",
+  linkedin: "", //!
+  github: "",
+}
