739d3e59c33e0b11efe9fa99d6af8b268c575847
sooheyon <shjo4732@gmail.com>
2023-07-20 12:56:50 +0900

    create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,17 @@
 import React from "react"
+import { bio } from "../../configs"
 import styled from "styled-components"
 
 const Footer = () => {
-  return <FooterStyled>index</FooterStyled>
+  const author = bio.author
+
+  return (
+    <FooterStyled>
+      © {new Date().getFullYear()} {author}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.com">Gatsby</a>
+    </FooterStyled>
+  )
 }
 
 const FooterStyled = styled.footer``
