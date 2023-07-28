191972a0ee58dd1beae9ac43def074668804e860
sooheyon <shjo4732@gmail.com>
2023-07-28 12:58:48 +0900

    footer 구조 변경

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,13 +7,30 @@ const Footer = () => {
 
   return (
     <FooterStyled>
-      © {new Date().getFullYear()} {author}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.com">Gatsby</a>
+      <div className="left-container">
+        {" "}
+        © {new Date().getFullYear()} {author}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </div>
+
+      <div className="right-container"></div>
     </FooterStyled>
   )
 }
 
-const FooterStyled = styled.footer``
+const FooterStyled = styled.footer`
+  display: flex;
+  flex-direction: row;
+
+  .left-container {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+
+  .right-container {
+  }
+`
 
 export default Footer
