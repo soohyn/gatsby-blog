3dc2e7649d9dc1689db0e1ba8cb1ddba4d58eba7
sooheyon <shjo4732@gmail.com>
2023-07-28 13:00:41 +0900

    footer 스타일 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
   return (
     <FooterStyled>
       <div className="left-container">
-        {" "}
-        © {new Date().getFullYear()} {author}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <div className="footer-copyright">
+          © {new Date().getFullYear()} {author}, Built with{" "}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </div>
       </div>
 
       <div className="right-container"></div>
@@ -27,9 +27,18 @@ const FooterStyled = styled.footer`
     display: flex;
     flex-direction: column;
     flex: 1;
+    padding: 20px;
+
+    > .footer-copyright {
+      display: flex;
+      flex-direction: row;
+    }
   }
 
   .right-container {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
   }
 `
 
