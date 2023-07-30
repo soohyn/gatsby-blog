456d04ca91d3d941667d2fc3d3f2464d9196a18a
sooheyon <shjo4732@gmail.com>
2023-07-30 10:52:25 +0900

    푸터 스타일 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { bio } from "../../configs"
 import styled from "styled-components"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 const Footer = () => {
   const author = bio.author
@@ -14,7 +15,30 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="right-container"></div>
+      <div className="right-container">
+        <div className="icon-list">
+          {bio?.github && (
+            <a
+              className="icon-wrap"
+              href={bio.github}
+              rel="external"
+              target="_blank"
+            >
+              <FaGithub size={20} />
+            </a>
+          )}
+          {bio?.linkedin && (
+            <a
+              className="icon-wrap"
+              href={bio.linkedin}
+              rel="external"
+              target="_blank"
+            >
+              <FaLinkedin size={20} />
+            </a>
+          )}
+        </div>
+      </div>
     </FooterStyled>
   )
 }
@@ -27,7 +51,7 @@ const FooterStyled = styled.footer`
     display: flex;
     flex-direction: column;
     flex: 1;
-    padding: 20px;
+    padding: 20px 40px;
 
     > .footer-copyright {
       display: flex;
@@ -38,7 +62,21 @@ const FooterStyled = styled.footer`
   .right-container {
     display: flex;
     flex-direction: column;
-    padding: 20px;
+    padding: 20px 40px;
+
+    .icon-list {
+      display: flex;
+      flex-direction: row;
+      padding: 12px 0px 0px 0px;
+
+      a.icon-wrap {
+        margin: 0px 0px 0px 8px;
+
+        &:nth-child(1) {
+          margin: 0;
+        }
+      }
+    }
   }
 `
 
